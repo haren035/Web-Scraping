280538aa6a871ef14fa4b6681c13b6495eaba94b
Haren Sah <haren3035@gmail.com>
2021-11-04 00:41:14 +1100

    filtering output by inputing unfamiliar skills.

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from bs4 import BeautifulSoup
-
+import time
 import requests
 
+print('Put some skills that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill} ')
+
 url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation='
-html_text = requests.get(url).text
-soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-skills = job.find('span', class_='srp-skills').text.replace(' ','')
-print(f'company name {company_name}')
-print(f'skills {skills}')
+
+def find_job():
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for job in jobs:
+        time_posted = job.find('span', class_='sim-posted').span.text
+        if 'few' in time_posted:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                print(f'company name: {company_name.strip()}')
+                print(f'skills: {skills.strip()}')
+                print(f'More info {more_info}')
+                print(' ')
 
 
-
+if __name__ == '__main__':
+    while True:
+        find_job()
+        time_wait = 10
+        print(f'waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60 )
 
